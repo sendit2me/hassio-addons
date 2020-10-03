38a9d4502fede7caaa43fefaaa4134ad60996e10
send_it_2_me_ <send_it_2_me_@hotmal.com>
2020-10-03 15:52:14 +0100

    tiy up run.sh

--- rclone/run.sh
+++ rclone/run.sh
@@ -2,26 +2,30 @@
 
 RCLONE_CONFIG=$(bashio::config 'configuration_path')
 REMOTES=$(bashio::config 'remotes')
-LOCAL_RETENTION=$(bashio::config 'local_retention_days')
-
-bashio::log.info "Pruning local files..."
-find /backup/ -mtime +${LOCAL_RETENTION} -type f -delete
-bashio::log.info "Pruning local files finished"
 
 for remote in ${REMOTES}; do
-    remote_name=$(bashio::jq "${remote}" ".name")
-    remote_path=$(bashio::jq "${remote}" ".path")
-    remote_retention=$(bashio::jq "${remote}" ".retention_days")
 
-    bashio::log.info "Starting remote copy for ${remote_name}..."
-    
+    remote_name=$(bashio::jq "${remote}" ".name")
+
+    remote_local_path=$(bashio::jq "${remote}" ".local_path")
+    remote_local_retention=$(bashio::jq "${remote}" ".local_retention_days")
+
+    remote_remote_path=$(bashio::jq "${remote}" ".remote_path")
+    remote_remote_retention=$(bashio::jq "${remote}" ".remote_retention_days")
+
+    bashio::log.info "Starting Remote copy for ${remote_name} ${remote_local_path} to ${remote_remote_path} ......"
+
+    bashio::log.info "Pruning local files... ${remote_local_path} till ${remote_local_retention} ....."
+    find ${remote_local_path} -mtime +${remote_local_retention} -type f -delete
+    bashio::log.info "Pruning local files ${remote_local_path} finished"
+
     rclone --config ${RCLONE_CONFIG} \
         sync -v \
-        --max-age "${remote_retention}d" \
+        --max-age "${remote_remote_retention}d" \
         --delete-during \
         --delete-excluded \
         --ignore-errors \
-        --ignore-existing /backup/ "${remote_name}:${remote_path}"
+        --ignore-existing "${remote_local_path}" "${remote_name}:${remote_remote_path}"
     
-    bashio::log.info "Remote copy for ${remote_name} finished"
+    bashio::log.info "Remote copy for ${remote_name} ${remote_local_path} to ${remote_remote_path} finished"
 done
